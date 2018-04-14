af3ea6e2aa96990e4c7da9269ff6ee2e1dd1aa72
wangningmt <37929293+wangningmt@users.noreply.github.com>
2018-04-14 21:55:34 +0800

    Add files via upload

---
+++ WindowsTest.java
@@ -0,0 +1,44 @@
+package MathProbGene;
+
+
+import java.util.Random;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class WindowsTest {
+
+	static long lCurrenTime = System.currentTimeMillis();
+	private static Random Rand = new Random(lCurrenTime);
+	
+	private static int intRand(int min,int max){
+		return min + Rand.nextInt( max - min + 1 ); 
+	}
+	private static int[] arrRand(){
+		int[] res = new int[6];
+		for(int i=0;i<6;++i)
+			res[i] = intRand(1, 100);
+		return res;
+	}
+	private static boolean[] brrRand(){
+		boolean[] res = new boolean[8];
+		for(int i=0;i<8;++i)
+			res[i] = Rand.nextBoolean();
+		return res;
+	}
+	@Test
+	public void testOnce() {
+		int[] arr = arrRand();
+		boolean[] brr = brrRand();
+		assertEquals( true, new Windows().Start(arr,brr));
+	}
+	@Test
+	public void testMore() {
+		int[] arr = arrRand();
+		boolean[] brr = brrRand();
+		for(int i=0;i<128;++i){	
+			for(int j=i,k=1;j>0;j/=2,k++) 
+				brr[k]=( j % 2 == 0 ? false : true );
+			assertEquals( true, new Windows().Start(arr,brr));
+		}
+	}
+}
